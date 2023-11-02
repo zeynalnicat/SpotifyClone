85f440ddb6b0ae10f7c3fa25c55d04c963c0d5f5
zeynalnicat <nicat21308@gmail.com>
2023-11-02 14:00:24 +0400

    SignUp4 fragment added, and its navigation in navigation component

--- app/src/main/java/com/example/spotifyclone/SignUp4.kt
+++ app/src/main/java/com/example/spotifyclone/SignUp4.kt
@@ -1,38 +1,27 @@
 package com.example.spotifyclone
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-
-import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.spotifyclone.databinding.FragmentSignUp3Binding
+import com.example.spotifyclone.databinding.FragmentSignUp4Binding
 
-class SignUp3 : Fragment() {
-    private lateinit var binding: FragmentSignUp3Binding
+class SignUp4 : Fragment() {
+    private lateinit var binding: FragmentSignUp4Binding
     private lateinit var btnList:MutableList<Button>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSignUp3Binding.inflate(inflater, container, false)
-
-        btnList = mutableListOf()
-        btnList.add(binding.btnMen)
-        btnList.add(binding.btnWomen)
-        btnList.add(binding.btnOther)
-
-        for (btn in btnList) {
-            btn.setOnClickListener {
-                Navigation.findNavController(requireView()).navigate(R.id.toSignUp4)
-            }
-        }
+        binding = FragmentSignUp4Binding.inflate(inflater, container, false)
 
         binding.navBack.setOnClickListener {
             val navController = Navigation.findNavController(requireActivity(), R.id.fragmentContainerView)
-            navController.popBackStack(R.id.signUp2, false)
+            navController.popBackStack(R.id.signUp3, false)
         }
 
         return binding.root
