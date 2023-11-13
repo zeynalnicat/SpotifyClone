28e7090f5a9e395c056097a2f25cb3dd8e6c3de5
Nicat Zeynalli <nicat21308@gmail.com>
2023-11-13 17:56:04 +0400

    Choose artist update (after 3 more selection of artist, next button reveal)

--- app/src/main/java/com/example/spotifyclone/Adapters/ArtistsAdapter.kt
+++ app/src/main/java/com/example/spotifyclone/Adapters/ArtistsAdapter.kt
@@ -1,23 +1,28 @@
 package com.example.spotifyclone.Adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.spotifyclone.Artists
 import com.example.spotifyclone.ChooseArtistPage.ChooseArtist
+
 import com.example.spotifyclone.R
 import com.example.spotifyclone.databinding.ArtistsListviewBinding
 import com.example.spotifyclone.databinding.FragmentChooseArtistBinding
 import java.util.Currency
 
-class ArtistsAdapter(private val artistList: MutableList<Artists>) :
+class ArtistsAdapter(private val artistList: MutableList<Artists> ,  private val fragment: Fragment) :
     RecyclerView.Adapter<ArtistsAdapter.AdapterHolder>() {
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterHolder {
         val binding =
             ArtistsListviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return AdapterHolder(binding)
+        return AdapterHolder(binding,parent)
     }
 
     override fun getItemCount(): Int {
@@ -29,34 +34,29 @@ class ArtistsAdapter(private val artistList: MutableList<Artists>) :
         return holder.bind(current)
     }
 
-    companion object {
-        private var selectionCount = 0
 
-        fun getSelectionCount(): Int {
-            return selectionCount
-        }
-    }
-
-    inner class AdapterHolder(private val binding: ArtistsListviewBinding) :
+    inner class AdapterHolder(private val binding: ArtistsListviewBinding,val parent: ViewGroup) :
         RecyclerView.ViewHolder(binding.root) {
 
 
         fun bind(current: Artists) {
-
             binding.imgChecked.visibility = if (current.isSelected) View.VISIBLE else View.INVISIBLE
             binding.txtArtistName.text = current.name
-            binding.imgArtist.setImageResource(current.imgSrc )
-
+            binding.imgArtist.setImageResource(current.imgSrc)
             binding.root.setOnClickListener {
                 current.isSelected = !current.isSelected
-                if (current.isSelected){
-                    selectionCount++
-                }else {
-                    selectionCount--
+                if (current.isSelected) {
+                    ChooseArtist.count ++
+                    Log.d("CountDebug", "Count: ${ChooseArtist.count}")
+                }else{
+                    ChooseArtist.count--
+                }
+
+                if (fragment is ChooseArtist) {
+                    fragment.updateButtonVisibility()
                 }
                 notifyDataSetChanged()
             }
-
 
 
         }
--- app/src/main/java/com/example/spotifyclone/ChooseArtistPage/ChooseArtist.kt
+++ app/src/main/java/com/example/spotifyclone/ChooseArtistPage/ChooseArtist.kt
@@ -2,6 +2,7 @@ package com.example.spotifyclone.ChooseArtistPage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +23,10 @@ class ChooseArtist : Fragment() {
     private lateinit var binding: FragmentChooseArtistBinding
     private lateinit var artistsList: MutableList<Artists>
 
+
+    companion object {
+        var count = 0
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -83,9 +88,9 @@ class ChooseArtist : Fragment() {
 
             )
 
-        val countListener = ArtistsAdapter.getSelectionCount()
 
-        binding.btnNext.visibility = if(countListener==0) View.VISIBLE else View.GONE
+
+
 
         binding.navBack.setOnClickListener {
             val navController = Navigation.findNavController(requireActivity(),
@@ -94,7 +99,7 @@ class ChooseArtist : Fragment() {
             navController.popBackStack(R.id.signUp43, false)
         }
 
-        val adapter = ArtistsAdapter(artistsList)
+        val adapter = ArtistsAdapter(artistsList,this)
         binding.artistRecycle.adapter = adapter
 
 
@@ -108,4 +113,11 @@ class ChooseArtist : Fragment() {
             return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        updateButtonVisibility()
+    }
+    fun updateButtonVisibility() {
+        binding.btnNext.visibility = if (count >= 3) View.VISIBLE else View.GONE
+    }
 }
