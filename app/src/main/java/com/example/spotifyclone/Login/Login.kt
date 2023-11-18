1f24481eb0fa54a682ab68c00b31d7eaa22dc2b8
Nicat Zeynalli <nicat21308@gmail.com>
2023-11-18 19:39:48 +0400

    Linking Login to Home Page

--- app/src/main/java/com/example/spotifyclone/Login/Login.kt
+++ app/src/main/java/com/example/spotifyclone/Login/Login.kt
@@ -1,11 +1,13 @@
 package com.example.spotifyclone.Login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.example.spotifyclone.HomeActivity
 import com.example.spotifyclone.R
 import com.example.spotifyclone.databinding.FragmentLoginBinding
 
@@ -22,6 +24,12 @@ class Login : Fragment() {
         binding.navBack.setOnClickListener {
             val navigation  = Navigation.findNavController(requireActivity(), R.id.fragmentContainerView)
             navigation.popBackStack(R.id.startFragment,false)
+        }
+
+        binding.btnNext.setOnClickListener{
+            val intent = Intent(requireActivity() , HomeActivity::class.java )
+            startActivity(intent)
+            requireActivity().finish()
         }
         return binding.root
     }
