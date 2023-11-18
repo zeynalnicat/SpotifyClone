b7976aac2c8b1314da13fd71dbf0f939e248bdfa
Nicat Zeynalli <nicat21308@gmail.com>
2023-11-18 19:47:16 +0400

    icons on top

---
+++ app/src/main/java/com/example/spotifyclone/Adapters/TrackAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.spotifyclone.Adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.spotifyclone.Tracks
+import com.example.spotifyclone.databinding.TrackListviewBinding
+
+class TrackAdapter(private val tracks: MutableList<Tracks>) :
+    RecyclerView.Adapter<TrackAdapter.TrackAdapterHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackAdapterHolder {
+        val binding =
+            TrackListviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrackAdapterHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
+    }
+
+    override fun onBindViewHolder(holder: TrackAdapterHolder, position: Int) {
+        val current = tracks[position]
+        return holder.bind(current)
+    }
+
+    inner class TrackAdapterHolder(private val binding: TrackListviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(current: Tracks) {
+            binding.imgArtist.setImageResource(current.imgSrc)
+            binding.txtArtistName.text = current.name
+            val params = binding.root.layoutParams as ViewGroup.MarginLayoutParams
+            params.rightMargin = 30
+            binding.root.layoutParams = params
+        }
+    }
+
+}
